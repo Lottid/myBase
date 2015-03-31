d8557847c6999bf75aa86a4392533ccefdfa769e
lottid <gyp19881020@sina.com>
2015-03-31 10:16:48 +0800

    myBase

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //合并js
+        concat: {
+            options: {
+                //separator: ';'
+            },
+            allInOne: { //所有JS文件全部合并成一份文件
+                src: ['dist/js/**/*.js'],
+                dest: 'dest/src-concated/js/<%= pkg.name %>.js'
+            }
+        },
+        //压缩js
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            buildall: { //按照原来的目录结构压缩所有JS文件
+                options: {
+                    mangle: true,
+                    compress: {
+                        drop_console: true
+                    },
+                    report: "min" //输出压缩率，可选的值有 false(不输出信息)，gzip
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist', //js目录
+                    src: '**/*.js', //所有js文件
+                    dest: 'dest/', //输出到此目录下
+                    ext: '.min.js' //指定扩展名
+                }]
+            }
+        },
+        //压缩css
+        cssmin: {
+            //文件头部输出信息
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                //美化代码
+                beautify: {
+                    //中文ascii化，非常有用！防止中文乱码的神配置
+                    ascii_only: true
+                }
+            },
+            my_target: {
+                files: [
+                    {
+                        expand: true,
+                        //相对路径
+                        cwd: 'dist/',
+                        src: '**/*.css',
+                        dest: 'dest/'
+                    }
+                ]
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                        flatten:true,
+                        expand: true,
+                        src: 'dist/img/**',
+                        dest: 'dest/img/',
+                        filter: 'isFile'
+                    } // 复制img目录下的所有文件
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['concat', 'uglify','cssmin','copy']);
+};
---
+++ dist/js/dist/js/asset/test.js
@@ -0,0 +1,4 @@
+function test() {
+	alert(123456);
+	console.log("123");
+}
