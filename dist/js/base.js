ba594532648b19c8957be70f9eeaf072a4e4eafc
Lottid <gyp19881020@sina.com>
2015-04-18 22:00:51 +0800

    add cookie

--- dist/js/base.js
+++ dist/js/base.js
@@ -89,9 +89,50 @@
             return elem.style.cssText;
         }
     }
+    /**
+     * [localData 操作localSorage]
+     * @fun  setCookie  设置
+     * @fun  getCookie  获取
+     * @fun  clearCookie  删除
+     */
     Base.cookie = {
-
+        //取得cookie  
+        getCookie:function(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';'); //把cookie分割成组  
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i]; //取得字符串  
+                while (c.charAt(0) == ' ') { //判断一下字符串有没有前导空格  
+                    c = c.substring(1, c.length); //有的话，从第二位开始取  
+                }
+                if (c.indexOf(nameEQ) == 0) { //如果含有我们要的name  
+                    return unescape(c.substring(nameEQ.length, c.length)); //解码并截取我们要值  
+                }
+            }
+            return false;
+        },
+        //清除cookie  
+        clearCookie:function(name) {
+            setCookie(name, "", -1);
+        },
+        //设置cookie  
+        setCookie:function(name, value, seconds) {
+            seconds = seconds || 0; //seconds有值就直接赋值，没有为0，这个根php不一样。  
+            var expires = "";
+            if (seconds != 0) { //设置cookie生存时间  
+                var date = new Date();
+                date.setTime(date.getTime() + (seconds * 1000));
+                expires = "; expires=" + date.toGMTString();
+            }
+            document.cookie = name + "=" + escape(value) + expires + "; path=/"; //转码并赋值  
+        }
     }
+    /**
+     * [localData 操作localSorage]
+     * @fun  set  设置
+     * @fun  get  获取
+     * @fun  remove  删除
+     */
     Base.localData = {
         isLocalStorage: window.localStorage ? true : false,
         set: function(key, value) {
