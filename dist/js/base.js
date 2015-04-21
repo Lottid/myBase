c0fcf62e065725b393921a38d4c982401c77814b
lottid <gyp19881020@sina.com>
2015-04-21 16:02:42 +0800

    array forEach 补丁

--- dist/js/base.js
+++ dist/js/base.js
@@ -15,6 +15,33 @@
         }
     }
 })(this, function(Base, undefined) {
+    //Array.forEach implementation for IE support..
+    //https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function(callback, thisArg) {
+            var T, k;
+            if (this == null) {
+                throw new TypeError(" this is null or not defined");
+            }
+            var O = Object(this);
+            var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+            if ({}.toString.call(callback) != "[object Function]") {
+                throw new TypeError(callback + " is not a function");
+            }
+            if (thisArg) {
+                T = thisArg;
+            }
+            k = 0;
+            while (k < len) {
+                var kValue;
+                if (k in O) {
+                    kValue = O[k];
+                    callback.call(T, kValue, k, O);
+                }
+                k++;
+            }
+        };
+    }
     /**
      * [hasClass 是否含有指定class]
      * @param  {[type]}  dom       [目标dom]
